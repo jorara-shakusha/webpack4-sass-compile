ad3e3925d5252dfd58a352d2f5d0377c93b63ec0
Fujita Kazuhiko <tutuhelvetica2@gmail.com>
2020-02-23 17:12:48 +0900

    mini-css-extract-plugin の導入。

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
 // 参考:最新版で学ぶwebpack 4入門 - スタイルシート（CSSやSass）を取り込む方法 - ICS MEDIA https://ics.media/entry/17376/
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = true;
+const path = require('path');
+const outputPath = path.resolve(__dirname, './dist');
 
 module.exports = {
     // モード値を production に設定すると最適化された状態で、
     // development に設定するとソースマップ有効でJSファイルが出力される
-    mode: "production",
+    // mode: "production",
+    mode: "development",
     // source-map 方式でないと、CSSの元ソースが追跡できないため
     devtool: "source-map",
+    entry: {
+      main: './src/index.js',
+    },
+    output: {
+      filename: '[name].js',
+      path: __dirname + '/dist/css'
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+    ],
     module: {
       rules: [
         {
           // 対象となるファイルの拡張子
-          test: /\.scss/,
+          test: /\.scss/, 
           // Sassファイルの読み込みとコンパイル
           use: [
-            // linkタグに出力する機能
-            "style-loader",
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
             // CSSをバンドルするための機能
             {
               loader: "css-loader",
@@ -46,5 +67,5 @@ module.exports = {
           ]
         }
       ]
-    }
+    },
   };
